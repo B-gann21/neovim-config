13bb477696ce1cfde0af8bcaaa204818a77c01d6
Brylan Gannon <brylan.gannon112@gmail.com>
2025-10-01 01:05:52 -0600

    Personalize commands

--- init.lua
+++ init.lua
@@ -1,15 +1,21 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
+
+local lsp = vim.lsp
+lsp.enable("ts_ls")
+lsp.enable("solargraph")
+lsp.enable("html")
+lsp.enable("lua_ls")
 
 require("vim-options")
 require("lazy").setup("plugins")
